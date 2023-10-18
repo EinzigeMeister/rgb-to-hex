8e937f5b91d0271a16dd5f35e628f7dd5b22d556
Eric Krukowski <erickruk@amazon.com>
2023-10-18 01:11:13 -0400

    completed all test cases

--- rgb-to-hex.py
+++ rgb-to-hex.py
@@ -1,1 +1,16 @@
+import pytest
 
+def rgb(r,g,b):
+   hex_strs = [hex(min(max(0,n), 255))[2:].upper() for n in [r, g, b]]
+   formatted_hex_strs = [h if len(h)==2 else "0" + h for h in hex_strs]
+   return formatted_hex_strs[0]+formatted_hex_strs[1]+formatted_hex_strs[2]
+
+@pytest.mark.parametrize('r, g, b, value', [
+      (0,0,0,"000000"),
+      (1,2,3,"010203"),
+      (255,255,255,"FFFFFF"),
+      (254,253,252,"FEFDFC"),
+      (-20,275,125,"00FF7D")
+])
+def test_rgb(r, g, b, value):
+        assert rgb(r,g,b) == value
