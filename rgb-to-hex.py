1a9fe7386a2344bc5bcac0ecf4e794b4f220bf9e
Eric Krukowski <erickruk@amazon.com>
2023-10-18 01:12:20 -0400

    add comments to code

--- rgb-to-hex.py
+++ rgb-to-hex.py
@@ -1,9 +1,9 @@
 import pytest
 
 def rgb(r,g,b):
-   hex_strs = [hex(min(max(0,n), 255))[2:].upper() for n in [r, g, b]]
-   formatted_hex_strs = [h if len(h)==2 else "0" + h for h in hex_strs]
-   return formatted_hex_strs[0]+formatted_hex_strs[1]+formatted_hex_strs[2]
+   hex_strs = [hex(min(max(0,n), 255))[2:].upper() for n in [r, g, b]] # turns int to a hex string, omitting the 0x at the begging
+   formatted_hex_strs = [h if len(h)==2 else "0" + h for h in hex_strs] # adds a leading 0 if none exists
+   return formatted_hex_strs[0]+formatted_hex_strs[1]+formatted_hex_strs[2] # concats and returns
 
 @pytest.mark.parametrize('r, g, b, value', [
       (0,0,0,"000000"),
